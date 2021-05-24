db7b60c3d9c95cdd48499ecd17c1feafc5c78d3f
vcaesar <vzvway@gmail.com>
2021-05-24 11:46:02 -0400

    add es client code

---
+++ es/es.go
@@ -0,0 +1,72 @@
+package es
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/olivere/elastic/v7"
+)
+
+type Conf struct {
+	URL     string `toml:"url" yaml:"url"`
+	Sniff   *bool  `toml:"sniff" yaml:"sniff"`
+	SetGzip *bool  `toml:"gzip" yaml:"gzip"`
+}
+
+type Client struct {
+	Client *elastic.Client
+	Conf   *Conf
+
+	ctx context.Context
+}
+
+func (c *Conf) Init() error {
+	s := true
+	if c.Sniff == nil {
+		c.Sniff = &s
+	}
+
+	if c.SetGzip == nil {
+		c.SetGzip = &s
+	}
+
+	return nil
+}
+
+func (c *Conf) New(ctx1 context.Context) (*Client, error) {
+	err := c.Init()
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := elastic.NewClient(
+		elastic.SetURL(c.URL),
+		elastic.SetSniff(*c.Sniff),
+		elastic.SetGzip(*c.SetGzip),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		Client: client,
+		Conf:   c,
+		ctx:    ctx1,
+	}, nil
+}
+
+func Source(q elastic.Query) (string, error) {
+	src, err := q.Source()
+
+	data, _ := json.Marshal(src)
+	return string(data), err
+}
+
+func Println(q elastic.Query) {
+	fmt.Println()
+
+	s, err := Source(q)
+	fmt.Println(s, err)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vcaesar/go-gse-elastic
+
+go 1.16
+
+require github.com/olivere/elastic/v7 v7.0.24
